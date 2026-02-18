46df0d9f1cdf0b4cfda794ba136d9d1f64200d72
willxxy <90741489+willxxy@users.noreply.github.com>
2026-02-17 20:39:28 -0500

    Add evaluation tasks and Weights & Biases logging

--- src/eval_encoder.py
+++ src/eval_encoder.py
@@ -7,15 +7,22 @@ from configs.config import get_args
 from utils.gpu_setup import GPUSetup, is_main
 from utils.seed_setup import set_seed
 from utils.eval_stats import aggregate_metrics
+from utils.wandb_setup import setup_wandb, cleanup_wandb, log_wandb
 
 from neural_networks.build_nn import BuildNN
 
 from dataloaders.build_dataloader import BuildDataLoader
 from runners.tasks.classification_eval import eval_classification
-
+from runners.tasks.generation_eval import eval_generation
+from runners.tasks.reconstruction_eval import eval_reconstruction
+from runners.tasks.forecasting_eval import eval_forecasting
 
 TASK_RUNNERS = {
-    "classification": eval_classification,
+    "multilabel_classification": eval_classification,
+    "multiclass_classification": eval_classification,
+    "generation": eval_generation,
+    "reconstruction": eval_reconstruction,
+    "forecasting": eval_forecasting,
 }
 
 
@@ -26,12 +33,19 @@ def main():
     args = get_args(mode)
     args.mode = mode
     assert args.task is not None, print("Please specify a task")
-    if is_main():
-        print(f"Evaluating {args.task}")
 
     if args.nn_ckpt:
+        data_names = "_".join(args.data)
+        batch_label_names = "_".join(args.batch_labels) if args.batch_labels else "None"
         run_dir = "/".join(args.nn_ckpt.split("/")[:-2])
         args.run_dir = run_dir
+    
+    if is_main():
+        print(f"Evaluating {args.task}")
+        if args.wandb:
+            name = f"{args.objective}_{batch_label_names}_{data_names}"
+            setup_wandb(args, name)
+
     eval_fn = TASK_RUNNERS[args.task]
 
     # seeds = [0, 1, 2, 3, 4]
@@ -52,11 +66,14 @@ def main():
 
         eval_result = eval_fn(nn, dataloader, args)
         all_metrics.append(eval_result)
+        if is_main() and args.wandb:
+            log_wandb(eval_result, prefix = f"seed_{seed}")
 
     results = aggregate_metrics(all_metrics)
+    if is_main() and args.wandb:
+        log_wandb(results, prefix = "agg")
+        cleanup_wandb()
     if args.nn_ckpt:
-        data_names = "_".join(args.data)
-        batch_label_names = "_".join(args.batch_labels)
         with open(f"{run_dir}/{args.task}_{batch_label_names}_{data_names}_results.json", "w") as f:
             json.dump(results, f, indent=2)
     else:
